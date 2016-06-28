9ddf1007aee57251310f1bd654f7cbbbfd2a1729
Nicolas Guillaume <gunicolas@sqli.com>
2016-06-28 09:31:44 +0200

    automatisation de l'installation du noeud geth dans docker

---
+++ docker/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "IP: "$IP
+
+#npm start --prefix $NODE_DATA_PATH &
+
+#geth --datadir $GETH_DATA_PATH --networkid "100" --port $GETH_PORT --ipcpath "../node/ethereum.ipc" --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" 
--- docker/xvfbRH.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
-
-Xvfb :99 -screen 0 1024x768x16  &> xvfb.log &
-
-npm start --prefix /home/node
