209bdc67a516f2cd3e2c1603f20b8422572ace70
Nicolas Guillaume <gunicolas@sqli.com>
2017-04-14 10:36:57 +0200

    add auto mining when pending transaction

--- docker/startup.sh
+++ docker/startup.sh
@@ -12,4 +12,17 @@ fi
 npm start --prefix $NODE_DATA_PATH &
 npm run server --prefix $NODE_DATA_PATH -- --port $NODE_BACK_PORT &
 
-geth --datadir $GETH_DATA_PATH --networkid $NETWORK_ID --port $GETH_PORT --ipcdisable --rpc --rpcaddr $RPC_ADDRESS --rpcport $RPC_PORT --rpccorsdomain "*" --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3"
+geth 	--datadir $GETH_DATA_PATH \
+		--networkid $NETWORK_ID \
+		--port $GETH_PORT \
+		--ipcdisable \
+		--rpc \
+		--rpcaddr $RPC_ADDRESS \
+		--rpcport $RPC_PORT \
+		--rpccorsdomain "*" \
+		--rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+		--preload $GETH_DATA_PATH/mine.js \
+		console
+
+
+
