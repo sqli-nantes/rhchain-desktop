7d680901a11b35778ff97e0905b78c52c30f7e47
Nigui <gunicolas@sqli.com>
2017-06-12 11:01:58 +0200

    remove bootnode from container + keep only admin mode

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-if [ ${BOOTNODE} ];then
-	sed -i -- 's#\[::\]#'$BOOTNODE'#g' $GETH_STATIC_NODE_FILE_PATH
-	rm -rf $GETH_DATA_PATH/geth/nodekey
-	echo "-------> NOT ADMIN MODE <--------"
-else
-	rm $GETH_STATIC_NODE_FILE_PATH
-	bootnode -nodekey $GETH_DATA_PATH/boot.key -verbosity 9 -v5 & #-addr ':'$BOOTNODE_TCP_PORT &
-	echo "BOOTNODE PUB : "$(cat $GETH_DATA_PATH/pub.key)
-	echo "-------> ADMIN MODE <--------"
-fi
-
 npm start --prefix $NODE_DATA_PATH &
 npm run server --prefix $NODE_DATA_PATH -- --port $NODE_BACK_PORT &
 
@@ -27,7 +16,7 @@ geth 	--datadir $GETH_DATA_PATH \
 		--preload $GETH_DATA_PATH/mine.js \
 		--v5disc \
 		--lightserv 90 \
-		--bootnodes="enode://"$(cat $GETH_DATA_PATH/pub.key)"@10.33.44.51:"$BOOTNODE_TCP_PORT"?discport:"$BOOTNODE_UDP_PORT \
+		--bootnodes=$BOOTNODE \
 		console
 
 
